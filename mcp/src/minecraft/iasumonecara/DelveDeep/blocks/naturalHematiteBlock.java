3f3415ea774c668e42189f51f655c2d797ebabcd
Kenneth A. Nall <iharenet@gmail.com>
2013-03-28 16:03:10 -0500

    Added base for natural blocks

---
+++ mcp/src/minecraft/iasumonecara/DelveDeep/blocks/naturalHematiteBlock.java
@@ -0,0 +1,23 @@
+package iasumonecara.DelveDeep.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+
+
+public class naturalHematiteBlock extends Block{
+
+	String texture;
+	
+	public naturalHematiteBlock(int par1, String texture) {
+		super(par1, Material.rock);
+		this.setCreativeTab(CreativeTabs.tabBlock);
+		this.texture = texture;
+		
+	}
+
+	public void registerIcons(IconRegister iconRegister) {
+		this.blockIcon = iconRegister.registerIcon("DelveDeep:NaturalBasalt");
+	}
+}
